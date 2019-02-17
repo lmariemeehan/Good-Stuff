cad013c5aa0b559ed38a1ef4d64f3273c0e25c12
Lola <lmariemeehan@gmail.com>
2019-02-17 11:49:56 -0800

    Minor tweak to express assignment but may need further update

--- src/routes/static.js
+++ src/routes/static.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit, marco polo!");
+  res.send("Welcome to Bloccit!");
 });
 
 module.exports = router;
