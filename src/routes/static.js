5cb22d2150653f6c9a1e239d5e85968cc01ddfa2
Lola <lmariemeehan@gmail.com>
2019-02-17 13:36:02 -0800

    Ensuring static assignment is up to date with checkpoint

--- src/routes/static.js
+++ src/routes/static.js
@@ -2,12 +2,6 @@ const express = require("express");
 const router = express.Router();
 const staticController = require("../controllers/staticController");
 
-<<<<<<< HEAD
-router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit!");
-});
-=======
 router.get("/", staticController.index);
->>>>>>> routingcontrollersview-checkpoint
 
 module.exports = router;
